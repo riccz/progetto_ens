a1bb8153f03d9dd9f4ba34acdf6f2dfa8920307c
Riccardo Zanol <ricc@zanol.eu>
2016-01-08 15:52:36 +0100

    Use only freq. f_0 from the first 4 seconds

--- find_noise_amplitude.m
+++ find_noise_amplitude.m
@@ -1,8 +1,12 @@
 function A = find_noise_amplitude(y, f_0, Fs)
 y_clean = y(1:4*Fs);
+Y_clean = 1/Fs * fft(y_clean);
 y_w_all_noises = y(length(y) - 4*Fs + 1: length(y));
-W = 1/Fs * (fft(y_w_all_noises) - fft(y_clean));
+Y_w_all_noises = 1/Fs * fft(y_w_all_noises);
 
 k = floor(f_0 * 4 + 0.5);
-A = abs(W(k+1)) * 2 / 4;
+Y_clean_f_0 = Y_clean(k+1);
+maxY_noises = Y_w_all_noises(k+1);
+
+A = abs(maxY_noises - Y_clean_f_0) * 2 / 4;
 end
